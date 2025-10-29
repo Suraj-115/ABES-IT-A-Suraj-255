0e7d333e899169dfc8d38ac238ca51719b2e4ca9
Suraj-115 <agarwalsuraj863@gmail.com>
2025-10-29 09:55:40 +0530

    React Card And Navbar

---
+++ react/react-practice/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from './Components/Card'
+import Navbar from './Components/Navbar'
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+      <br />
+      <Card/>
+      <br />
+      <Card/>
+    </>
+  )
+}
+
+export default App
