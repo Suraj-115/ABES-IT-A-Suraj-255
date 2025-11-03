4063a3cafd84231ffac4a9a4033ba11047510620
Suraj-115 <agarwalsuraj863@gmail.com>
2025-11-03 12:50:10 +0530

    React Book App Starting

---
+++ react/book-app/src/Component/Book.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./book.css";
+const Book = () => {
+  return (
+    <div id="book">
+      <img src="" alt=""/>
+      <h1>Title:Math</h1>
+      <h1>250</h1>
+    </div>
+  )
+}
+
+export default Book
